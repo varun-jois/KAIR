1cd3999409352cd5bc9e353f048796465e09b338
varun-jois <varunrameshjois@gmail.com>
2021-11-13 23:01:18 -0500

    Manually changed the hard ratio from 1 to 2.

--- models/model_plain_aug.py
+++ models/model_plain_aug.py
@@ -29,7 +29,7 @@ class ModelPlainAug(ModelBase):
         self.netG = self.model_to_device(self.netG)
         self.netA = define_A(opt)
         self.netA = self.model_to_device(self.netA)
-        self.hard_ratio = 1
+        self.hard_ratio = 2
         self.augmentation_wt = 1
         if self.opt_train['E_decay'] > 0:
             self.netE = define_G(opt).to(self.device).eval()
