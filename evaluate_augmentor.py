7a81de1fb3a1262494398ef4b1d374f4f6ef7001
varun-jois <varunrameshjois@gmail.com>
2022-02-12 16:21:17 -0500

    Modified evaluator.

--- evaluate_augmentor.py
+++ evaluate_augmentor.py
@@ -21,7 +21,7 @@ def compare_augmentor_models():
     # image paths
     paths = util.get_image_paths('/home/varun/sr/datasets/DIV2K/DIV2K_valid_HR_randSample')
     # hr_steps = {1: 40000, 4: 65000, 16: 68000, 64: 72000, 256: 76000, 1024: 80000, 4096: 84000, 16384: 88000}
-    hr_steps = {'1K': 1000, '7k': 7_000, '20k': 20_000}
+    hr_steps = {'1K': 1000, '7k': 7_000}
     dir = '/home/varun/sr/KAIR/aug_images'
 
     # the images to evaluate
