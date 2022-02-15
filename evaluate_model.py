4217a496b1cc0c382f5f68b56f4315cb6fc5ecc9
varun-jois <varunrameshjois@gmail.com>
2022-02-14 21:48:28 -0500

    Fixed a bug. (3)

--- evaluate_model.py
+++ evaluate_model.py
@@ -83,6 +83,9 @@ def main(model_names):
                 img_H_path = os.path.join(H_path_dir, f'{name[:-2]}.png')
                 img_H = util.imread_uint(img_H_path, 3)
 
+                print(f'E shape {img_E.shape}')
+                print(f'E shape {img_H.shape}')
+
                 # calculate the metrics
                 psnr_total += util.calculate_psnr(img_E, img_H, border=border)
                 ssim_total += util.calculate_ssim(img_E, img_H, border=border)
