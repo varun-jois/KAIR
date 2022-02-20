d018c8b0a7ea24f76807dc6e2ba41bde1ec71deb
varun-jois <varunrameshjois@gmail.com>
2022-02-20 11:59:31 -0500

    Made changes for a new loss. (2)

--- evaluate_augmentor.py
+++ evaluate_augmentor.py
@@ -38,7 +38,7 @@ def compare_augmentor_models(device, model_name):
         aug = RRDBNET_AUG()
         aug = aug.to(device)
         state_dict = torch.load(os.path.join(model_dir, model_name))
-        aug.load_state_dict(state_dict)
+        aug.load_state_dict(state_dict, strict=False)
 
         # load the generator
         model_name = f'{step}_G.pth'
