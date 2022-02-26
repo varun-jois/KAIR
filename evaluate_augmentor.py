a6fd76d9bdada21ef35d28da5735c81f8e8e4275
varun-jois <varunrameshjois@gmail.com>
2022-02-26 11:00:32 -0500

    Added perceptual loss.

--- evaluate_augmentor.py
+++ evaluate_augmentor.py
@@ -24,7 +24,8 @@ def compare_augmentor_models(device, model_name):
     # image paths
     paths = util.get_image_paths('/home/varun/sr/datasets/DIV2K/DIV2K_valid_HR_randSample')
     # hr_steps = {1: 40000, 4: 65000, 16: 68000, 64: 72000, 256: 76000, 1024: 80000, 4096: 84000, 16384: 88000}
-    hr_steps = {'125ep_hr1': 125, '150ep_hr1': 150}
+    # hr_steps = {'125ep_hr1': 125, '150ep_hr1': 150, }
+    hr_steps = {f'{i}ep': i for i in range(125, 325, 25)}
     dir = '/home/varun/sr/KAIR/aug_images'
     model_dir = f'/home/varun/sr/KAIR/superresolution/{model_name}/models'
 
